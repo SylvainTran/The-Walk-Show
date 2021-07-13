e458d90ac123bd9ea514525544d46fb8403a230b
SylvainTran <45033804+SylvainTran@users.noreply.github.com>
2021-07-13 13:57:59 -0400

    Added sound effects for newborn's gender toggles, and narrators reading their respective wikipedia entry; Refactored codebase into MVC architecture; Added support for newborn head and torso mesh changes;  Added placeholder psychological and socioeconomic edit pages
    I started adding sounds and narration prematurely to investigate whether the experience of sampling through a newborn's gender options could already cause a different qualia or sensation (See repos' conversations wiki page for more details) before committing to add many more customization choices. The narrator reads the wiki's first paragraph description of each option, using Microsoft's Azure neural AI-based text-to-speech that I recorded using Audacity. The desired effect is to slow down the selection process through the edits to pay attention to what is being said, and to be more deliberate about each choice. I may have to add the text to the side in the future if the effect doesn't quite work or the content of the speech is not as comprehensible as intended. To test later. Another change is that I put discrete options for the newborn's head and torso mesh. Is this necessary? That's also unknown, although the intention was to make the player be more attached/involved with their newborn. Although highly strange, it also allows the possibility of adding a slider for genital size to drive home the point of the strangeness of gene editing a new born's biology and could ostentially be humorous. The last design specific remark is on the two new edit tabs: psychological and socioeconomic. These were inevitable as they represent two other big areas of influence in a person's life, although I'm not sure if I should change the terms belonging to real-life into more game-design appropriate, accessible terms. For example, instead of "temperament" or "extraversion/introversion" traits, the game version might be "charisma" or more RPG-like terms such as "leader" or "loud vs. silent". For occupations in the socioeconomic tab, it's unsure if it should be kept and whether the terms need to match real-life occupations as well - this is a theme issue. For now, the theme is a sci-fi futuristic Mars colonization theme (or possibly another galaxy, etc.), so occupations may need to match that speculative era. Right now they start from the stone age?

--- V00717/Assets/Scripts/BabyModel.cs
+++ V00717/Assets/Scripts/BabyModel.cs
@@ -1,6 +1,7 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-// The baby's data
+// The baby's data - also saves and retrieves data to database/json file
 public class BabyModel : MonoBehaviour
 {
     // The baby's sex.
@@ -13,58 +14,21 @@ public class BabyModel : MonoBehaviour
     public float AdultHeight { get { return adultHeight; } set { adultHeight = value; } }
     // The R value of the material for the skin
     private float skinColorR = 0.0f;
-    public float SkinColorR { set { skinColorR = value; } }
+    public float SkinColorR { get { return skinColorR; } set { skinColorR = value; } }
     // The G value of the material for the skin
     private float skinColorG = 0.0f;
-    public float SkinColorG { set { skinColorG = value; } }
+    public float SkinColorG { get { return skinColorG; } set { skinColorG = value; } }
     // The B value of the material for the skin
     private float skinColorB = 0.0f;
-    public float SkinColorB { set { skinColorB = value; } }
+    public float SkinColorB { get { return skinColorB; } set { skinColorB = value; } }
 
-    //Setter for baby's sex via Unity's built-in event system.
-    public void OnSexChanged(string sex)
-    {
-        this.sex = sex;
-        Debug.Log($"Baby's sex was changed to: {sex}");
-    }
+    // The currently used mesh names for head and torso - to allow View to reload the proper mesh
+    private string activeHeadName;
+    public string ActiveHeadName { get { return activeHeadName; } set{ activeHeadName = value; } }
+    private string activeTorsoName;
+    public string ActiveTorsoName { get { return activeTorsoName; } set { activeTorsoName = value; } }
 
-    //Setter for baby's adult height via Unity's built-in event system.
-    public void OnAdultHeightChanged(float adultHeight)
-    {
-        this.adultHeight = adultHeight;
-        Debug.Log($"Baby's adult height was changed to: {adultHeight}");
-    }
-
-    //Setter for skin color changed (red slider)
-    public void OnSkinColorChanged_R(float r)
-    {
-        skinColorR = r;
-        UpdateSkinColor();
-    }
-
-    //Setter for skin color changed (green slider)
-    public void OnSkinColorChanged_G(float g)
-    {
-        skinColorG = g;
-        UpdateSkinColor();
-    }
-
-    //Setter for skin color changed (blue slider)
-    public void OnSkinColorChanged_B(float b)
-    {
-        skinColorB = b;
-        UpdateSkinColor();
-    }
-
-    // Procedure to update the skin color of this game object's children
-    // Gets the renderer of this go's children and updates its materials with the new rgb values
-    public void UpdateSkinColor()
-    {
-        Renderer rendHead = transform.GetChild(0).GetComponent<Renderer>();
-        Renderer rendTorso = transform.GetChild(1).GetComponent<Renderer>();
-        Material newMat = new Material(Shader.Find("Standard"));
-        newMat.SetColor("_Color", new Color(skinColorR, skinColorG, skinColorB));
-        rendHead.material = newMat;
-        rendTorso.material = newMat;
-    }
+    // Meshes for head and torso
+    public List<GameObject> heads;
+    public List<GameObject> torsos;
 }
