eb35724644766c60b4dee388b8a2b532c0509108
Sylvain Tran <sylvain.l.tran@gmail.com>
2021-08-24 22:35:06 -0400

    Revamped main menu; Added cinematic intro/tutorial scenes 1 and 2; Added dynamic load game button;  Redesigned chat page/mind chat as Virtual Bar with 3D bar
    Sugar coating the stuff so it's closer to the real stuff.

--- V00717/Assets/Scripts/SceneController.cs
+++ V00717/Assets/Scripts/SceneController.cs
@@ -2,22 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneController : MonoBehaviour
 {
+    public Button loadGameButton;
+
+    private void Start()
+    {
+        if (SaveSystem.SaveFileExists("PlayerStatistics.json") || SaveSystem.SaveFileExists("colonists.json") || SaveSystem.SaveFileExists("deadColonists.json"))
+        {
+            loadGameButton.GetComponent<Button>().interactable = true;
+        } else
+        {
+            loadGameButton.GetComponent<Button>().interactable = false;
+        }
+    }
     public void NewGame()
     {
         // Todo create new save slot
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void LoadGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(3);
     }
 
     public void PatronsGallery()
     {
-        SceneManager.LoadScene(2);
+
     }
 }
